ef5d4d3096cf10c6c2d254a5b16bf6f351e20a02
uploadbakonbox <48163056+uploadbakonbox@users.noreply.github.com>
2019-03-02 12:50:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var myName = "Mr. Watson";
+
+var red = [100, 0, 0];
+var orange = [189, 94, 0];
+var green = [0, 100, 0];
+var blue = [0, 0, 100];
+var purple = [104, 14, 156];
+var letterColors = [red, orange, green, blue, purple];
+
+drawName(myName, letterColors);
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
