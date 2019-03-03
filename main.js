f1f2847a39bd49ecf0e19a9c0a28e1d3301f22e9
uploadbakonbox <48163056+uploadbakonbox@users.noreply.github.com>
2019-03-02 18:16:11 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var myName = "Mr. Watson";
 
-var c1 = [80, 0, 0];
-var c2 = [80, 15, 0];
-var c3 = [0, 80, 0];
-var c4 = [0, 0, 80];
-var c5 = [35, 0, 80];
+var c1 = [222, 222, 222];
+var c2 = [222, 222, 222];
+var c3 = [222, 222, 222];
+var c4 = [222, 222, 222];
+var c5 = [222, 222, 222];
 var letterColors = [c1, c2, c3, c4, c5];
 
 drawName(myName, letterColors);
