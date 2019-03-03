d4507ed80029d94787522edb8d16db8716bcf000
uploadbakonbox <48163056+uploadbakonbox@users.noreply.github.com>
2019-03-02 17:55:29 -0700

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -156,8 +156,8 @@ function initEventListeners() {
  
 function updateCanvasDimensions() {
     canvas.attr({
-        height: 500,
-        width: 1000
+        height: 400,
+        width: 800
     });
     canvasWidth = canvas.width();
     canvasHeight = canvas.height();
