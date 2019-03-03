6134fef008fac6d81c29740b55c23968cb042e56
uploadbakonbox <48163056+uploadbakonbox@users.noreply.github.com>
2019-03-02 18:20:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 var myName = "Mr. Watson";
 
-var c1 = [222, 222, 222];
-var c2 = [222, 222, 222];
-var c3 = [222, 222, 222];
-var c4 = [222, 222, 222];
-var c5 = [222, 222, 222];
-var letterColors = [c1, c2, c3, c4, c5];
+var red = [0, 20, 62];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
 
 drawName(myName, letterColors);
 
-// bubbleShape = 'square';
-
-bubbleShape = 'circle';
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
 
 bounceBubbles();
